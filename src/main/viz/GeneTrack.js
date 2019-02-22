03afc76aba2cd4c6beda66e6fdc1583c73ee4805
Gene Selkov <gene.selkov@tempus.com>
2019-02-22 16:45:27 -0600

    merge with upstream

--- src/main/viz/GeneTrack.js
+++ src/main/viz/GeneTrack.js
@@ -8,6 +8,10 @@ import type {Strand} from '../Alignment';
 import type {Gene, BigBedSource} from '../sources/BigBedDataSource';
 import type {VizProps} from '../VisualizationWrapper';
 import type {Scale} from './d3utils';
+import type {State} from '../types';
+
+import GenericFeature from '../data/genericFeature';
+import {GenericFeatureCache} from './GenericFeatureCache';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -20,6 +24,7 @@ import d3utils from './d3utils';
 import scale from '../scale';
 import ContigInterval from '../ContigInterval';
 import canvasUtils from './canvas-utils';
+import utils from '../utils';
 import dataCanvas from 'data-canvas';
 import style from '../style';
 import copyToClipboard from '../Clipboard';
@@ -58,7 +63,8 @@ function drawGeneName(ctx: CanvasRenderingContext2D,
                       textIntervals: Interval[]) {
   var p = gene.position,
       centerX = 0.5 * (clampedScale(1 + p.start()) + clampedScale(1 + p.stop()));
-  var name = gene.name || gene.id;
+  // do not use gene name if it is null or empty
+  var name =  !_.isEmpty(utils.stringToLiteral(gene.name)) ? gene.name : gene.id;
   var textWidth = ctx.measureText(name).width;
   var textInterval = new Interval(centerX - 0.5 * textWidth,
                                   centerX + 0.5 * textWidth);
@@ -69,13 +75,15 @@ function drawGeneName(ctx: CanvasRenderingContext2D,
   }
 }
 
-class GeneTrack extends React.Component {
-  props: VizProps & { source: BigBedSource };
-  state: {genes: Gene[]};
+class GeneTrack extends React.Component<VizProps<BigBedSource>, State> {
+  props: VizProps<BigBedSource>;
+  state: State;
+  cache: GenericFeatureCache;
 
-  constructor(props: VizProps) {
+  constructor(props: VizProps<BigBedSource>) {
     super(props);
     this.state = {
+      networkStatus: null,
       genes: []
     };
   }
@@ -85,10 +93,26 @@ class GeneTrack extends React.Component {
   }
 
   componentDidMount() {
-    // Visualize new reference data as it comes in from the network.
+    this.cache = new GenericFeatureCache(this.props.referenceSource);
+      // Visualize new reference data as it comes in from the network.
     this.props.source.on('newdata', (range) => {
+      // add genes to generic cache
+      var genes = this.props.source.getFeaturesInRange(range);
+      genes.forEach(f => this.cache.addFeature(new GenericFeature(f.id, f.position, f)));
+      this.updateVisualization();
+    });
+    this.props.referenceSource.on('newdata', range => {
+      this.updateVisualization();
+    });
+    this.props.source.on('networkprogress', e => {
+      this.setState({networkStatus: e});
+    });
+    this.props.source.on('networkdone', () => {
       this.setState({
-        genes: this.props.source.getGenesInRange(range)
+        networkStatus: null,
+        genes: this.props.source.getFeaturesInRange(
+          new ContigInterval(range.contig, range.start, range.stop)
+        )
       });
     });
 
@@ -100,8 +124,8 @@ class GeneTrack extends React.Component {
   }
 
   componentDidUpdate(prevProps: any, prevState: any) {
-    if (!shallowEquals(prevProps, this.props) ||
-        !shallowEquals(prevState, this.state)) {
+    if (!shallowEquals(this.props, prevProps) ||
+        !shallowEquals(this.state, prevState)) {
       this.updateVisualization();
     }
   }
@@ -123,50 +147,59 @@ class GeneTrack extends React.Component {
             .range([0, width])
             .clamp(true);
 
-    d3utils.sizeCanvas(canvas, width, height);
 
-    var ctx = dataCanvas.getDataContext(canvasUtils.getContext(canvas));
-    ctx.reset();
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    var geneLineY = Math.round(height / 4);
-    var textIntervals = [];  // x-intervals with rendered gene names, to avoid over-drawing.
-    // TODO: don't pull in genes via state.
-    ctx.font = `${style.GENE_FONT_SIZE}px ${style.GENE_FONT}`;
-    ctx.textAlign = 'center';
-    this.state.genes.forEach(gene => {
-      if (!gene.position.chrIntersects(range)) return;
-      ctx.pushObject(gene);
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = style.GENE_COLOR;
-      ctx.fillStyle = style.GENE_COLOR;
+    if (canvas && canvas instanceof Element) { // check for getContext
+      if (canvas instanceof HTMLCanvasElement) { // check for sizeCanvas
+        d3utils.sizeCanvas(canvas, width, height);
+      }
+      var ctx = dataCanvas.getDataContext(canvasUtils.getContext(canvas));
 
-      canvasUtils.drawLine(ctx, clampedScale(1 + gene.position.start()), geneLineY + 0.5,
-                                clampedScale(1 + gene.position.stop()), geneLineY + 0.5);
+      ctx.reset();
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-      // TODO: only compute all these intervals when data becomes available.
-      var exons = bedtools.splitCodingExons(gene.exons, gene.codingRegion);
-      exons.forEach(exon => {
-        ctx.fillRect(sc(1 + exon.start),
-                     geneLineY - 3 * (exon.isCoding ? 2 : 1),
-                     sc(exon.stop + 2) - sc(1 + exon.start),
-                     6 * (exon.isCoding ? 2 : 1));
+      var geneLineY = Math.round(height / 4);
+      var textIntervals = [];  // x-intervals with rendered gene names, to avoid over-drawing.
+      ctx.font = `${style.GENE_FONT_SIZE}px ${style.GENE_FONT}`;
+      ctx.textAlign = 'center';
+      var vFeatures = _.flatten(_.map(this.cache.getGroupsOverlapping(range),
+                    g => g.items));
+
+      vFeatures.forEach(vFeature => {
+        var gene = vFeature.gFeature;
+        if (!gene.position.intersects(range)) return;
+        ctx.pushObject(gene);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = style.GENE_COLOR;
+        ctx.fillStyle = style.GENE_COLOR;
+
+        canvasUtils.drawLine(ctx, clampedScale(1 + gene.position.start()), geneLineY + 0.5,
+                                  clampedScale(1 + gene.position.stop()), geneLineY + 0.5);
+
+        // TODO: only compute all these intervals when data becomes available.
+        var exons = bedtools.splitCodingExons(gene.exons, gene.codingRegion);
+        exons.forEach(exon => {
+          ctx.fillRect(sc(1 + exon.start),
+                       geneLineY - 3 * (exon.isCoding ? 2 : 1),
+                       sc(exon.stop + 2) - sc(1 + exon.start),
+                       6 * (exon.isCoding ? 2 : 1));
+        });
+
+        var introns = gene.position.interval.complementIntervals(gene.exons);
+        introns.forEach(range => {
+          drawArrow(ctx, clampedScale, range, geneLineY + 0.5, gene.strand);
+        });
+        ctx.strokeStyle = style.GENE_COMPLEMENT_COLOR;
+        ctx.lineWidth = 2;
+        gene.exons.forEach(range => {
+          drawArrow(ctx, clampedScale, range, geneLineY + 0.5, gene.strand);
+        });
+
+        drawGeneName(ctx, clampedScale, geneLineY, gene, textIntervals);
+
+        ctx.popObject();
       });
-
-      var introns = gene.position.interval.complementIntervals(gene.exons);
-      introns.forEach(range => {
-        drawArrow(ctx, clampedScale, range, geneLineY + 0.5, gene.strand);
-      });
-      ctx.strokeStyle = style.GENE_COMPLEMENT_COLOR;
-      ctx.lineWidth = 2;
-      gene.exons.forEach(range => {
-        drawArrow(ctx, clampedScale, range, geneLineY + 0.5, gene.strand);
-      });
-
-      drawGeneName(ctx, clampedScale, geneLineY, gene, textIntervals);
-
-      ctx.popObject();
-    });
+    } // end typecheck for canvasß
   }
 
   handleClick(reactEvent: any) {
